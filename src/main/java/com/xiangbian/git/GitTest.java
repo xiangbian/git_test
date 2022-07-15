d94320cf17738815ee2bdc5ff5e59dac314eb64f
halo <halo@163.com>
2022-07-15 12:12:56 +0800

    second commit

--- src/main/java/com/xiangbian/git/GitTest.java
+++ src/main/java/com/xiangbian/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xiangbian.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git test~");
+        System.out.println("Git test2~");
     }
 }

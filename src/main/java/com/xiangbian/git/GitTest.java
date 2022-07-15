79d0f758ef13674b91759a1e1917ee01601be67a
halo <halo@163.com>
2022-07-15 12:14:36 +0800

    third commit

--- src/main/java/com/xiangbian/git/GitTest.java
+++ src/main/java/com/xiangbian/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git test~");
         System.out.println("Git test2~");
+        System.out.println("Git test3~");
     }
 }
